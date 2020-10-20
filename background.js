e1e3427fac49659e546c9a6d6d433c8843ecabed
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-10-20 18:13:53 +0200

    make the icon red if the vpn is not connected

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
 var port = chrome.runtime.connectNative('com.stwunsch.nordvpn_status');
 var data = null;
+var state = null;
 
 port.onMessage.addListener(function(msg) {
   data = msg;
+  var new_state = true;
+  if (data && (!data.success || (data.success && data["Status"] != "Connected"))) {
+      new_state = false;
+  }
+  if (state == null || new_state != state) {
+    state = new_state;
+    var tag = "";
+    if (!state) tag = "-red";
+    chrome.browserAction.setIcon({
+      path:
+        {
+          19: "icon" + tag + "-19.png",
+          48: "icon" + tag + "-48.png",
+          128: "icon" + tag + "-128.png"
+        }
+    })
+  }
 });
